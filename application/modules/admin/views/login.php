2dd855298dd4e12b16208cee0850761b8e1e5aa4
Logesh K <logeshkrishnan5@gmail.com>
2022-09-19 06:46:56 +0530

    changes made in design

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -1,24 +1,33 @@
-<div class="login-box">
-
-	<div class="login-box-body">
-		<span class="sign-in-text">Sign in</span>
-		<div> </div>
-		<span class="">GRS Hiring Portal </span>
+<div class="row ">
+<div class="col-md-4"></div>
+<div class="col-md-4 ">
+	<div class="login-box-body vertical-center">
+		<span class="sign-in-text-bld">Sign In </span>
+		<div></div>
+		<span class="sign-in-text">to GRS Hiring Portal</b></span>
+		<div class="custom-height"></div>
+		
 		<?php echo $form->open(); ?>
 			<?php echo $form->messages(); ?>
-			<?php echo $form->bs3_text('', 'username', ENVIRONMENT==='development' ? 'Email ID' : ''); ?>
-			<?php echo $form->bs3_password('', 'password', ENVIRONMENT==='development' ? 'password' : ''); ?>
+			<div class="txt_field">
+			<?php echo $form->bs3_text('Username / Email Address*', 'username', ENVIRONMENT==='development' ? ' ' : ''); ?>
+			</div>
+			<div class="txt_field">
+			<?php echo $form->bs3_password('Password', 'password', ENVIRONMENT==='development' ? '' : ''); ?>
+			</div>
 			<div class="row">
 				<div class="col-xs-8">
-					<div class="checkbox">
+				<div class="checkbox">
 						<label><input type="checkbox" name="remember"> Remember Me</label>
-					</div>
 				</div>
-				<div class="col-xs-4">
-					<?php echo $form->bs3_submit('Sign In', 'btn btn-primary btn-block btn-flat hp-login-btn'); ?>
+</div>
+				<div class="col-xs-4"><br>
+					<?php echo $form->bs3_submit('LOGIN', 'btn btn-primary btn-block btn-flat hp-login-btn'); ?>
 				</div>
 			</div>
 		<?php echo $form->close(); ?>
 	</div>
 
+</div>
+<div class="col-md-4"></div>
 </div>
