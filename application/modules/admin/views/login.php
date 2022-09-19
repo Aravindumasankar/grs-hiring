c091cc8cb6c7463e82032816418c1c50d02762a9
Aravind Umasankar <aravind-umasankar@hpe.com>
2022-09-19 06:58:57 +0530

    Update login.php

--- application/modules/admin/views/login.php
+++ application/modules/admin/views/login.php
@@ -1,33 +1,24 @@
-<div class="row ">
-<div class="col-md-4"></div>
-<div class="col-md-4 ">
-	<div class="login-box-body vertical-center">
-		<span class="sign-in-text-bld">Sign In </span>
-		<div></div>
-		<span class="sign-in-text">to GRS Hiring Portal</b></span>
-		<div class="custom-height"></div>
-		
+<div class="login-box">
+
+	<div class="login-logo"><b><?php echo $site_name; ?></b></div>
+
+	<div class="login-box-body">
+		<p class="login-box-msg">Sign in to start your session</p>
 		<?php echo $form->open(); ?>
 			<?php echo $form->messages(); ?>
-			<div class="txt_field">
-			<?php echo $form->bs3_text('Username / Email Address*', 'username', ENVIRONMENT==='development' ? ' ' : ''); ?>
-			</div>
-			<div class="txt_field">
-			<?php echo $form->bs3_password('Password', 'password', ENVIRONMENT==='development' ? '' : ''); ?>
-			</div>
+			<?php echo $form->bs3_text('Username', 'username', ENVIRONMENT==='development' ? 'webmaster' : ''); ?>
+			<?php echo $form->bs3_password('Password', 'password', ENVIRONMENT==='development' ? 'webmaster' : ''); ?>
 			<div class="row">
 				<div class="col-xs-8">
-				<div class="checkbox">
+					<div class="checkbox">
 						<label><input type="checkbox" name="remember"> Remember Me</label>
+					</div>
 				</div>
-</div>
-				<div class="col-xs-4"><br>
-					<?php echo $form->bs3_submit('LOGIN', 'btn btn-primary btn-block btn-flat hp-login-btn'); ?>
+				<div class="col-xs-4">
+					<?php echo $form->bs3_submit('Sign In', 'btn btn-primary btn-block btn-flat'); ?>
 				</div>
 			</div>
 		<?php echo $form->close(); ?>
 	</div>
 
-</div>
-<div class="col-md-4"></div>
 </div>
